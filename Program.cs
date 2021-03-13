98e0fa6c0297c80a8496f4ca4adf9420a542d31b
Cross-bit <ondra.kryz@gmail.com>
2021-03-13 12:28:51 +0100

    comment

--- Program.cs
+++ Program.cs
@@ -4,18 +4,18 @@ using System.Collections.Generic;
 
 namespace permutator
 {
-    class Car{
+    /*class Car{
         public string color = "";
         public Car(string color) {
             this.color = color;
         }
-    }
+    }*/
 
     class Program
     {
         static void Main(string[] args) {
 
-            /*IPermutator<Car> permutator = new Permutator<Car>(new Car[3] { new Car("R"), new Car("G"), new Car("B") });
+            /*Permutator<Car> permutator = new Permutator<Car>(new Car[3] { new Car("R"), new Car("G"), new Car("B") });
             List<Car[]> permutations = permutator.GetPermutations;
             Console.WriteLine(permutator.GetNumOfPerms);
 
